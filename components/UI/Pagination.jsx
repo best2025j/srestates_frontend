29741a93b32494a511eef50abacfc662566766b5
Toyinso <116766819+Toyinso@users.noreply.github.com>
2023-03-26 02:05:43 -0700

    pagination done

--- components/UI/Pagination.jsx
+++ components/UI/Pagination.jsx
@@ -1,31 +1,52 @@
-import React from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import Image from "next/image";
+import React from "react";
+import man from "../../public/assets/images/man.png";
+import { Button } from "@mantine/core";
 
 const Pagination = () => {
   return (
-    <div>
-      <div>
-        <h1>Check Out Our Recent Reviews</h1>
-        <p>
+    <div className="h-full w-full py-20">
+      <div className="text-center space-y-[10px]">
+        <div className="">
+          <h1 className="text-[3.2rem] font-bold">
+            Check Out Our Recent Reviews
+          </h1>
+        </div>
+        <p className="font-normal text-2xl">
           Our reviews are from individuals like you who have similar interest.
         </p>
       </div>
 
-      <div>
-        <h1>Found A Roommate</h1>
-        <button>
-          <FaAngleLeft />
-        </button>
-        <button>
-          <FaAngleRight />
-        </button>
-        <blockquote>
-          “I’m very pleased with this website. I came across it 2022 and I had
-          no regrets whatsoever. I’ve recommended it to so many friends and
-          family. I got my roommate from here and there is no issue. I just wish
-          Studentrealestates becomes viral because it makes roommate pairing
-          easier.”
-        </blockquote>
+      <div className="flex text-center justify-center py-16 items-center h-full w-full gap-28 ">
+        <Button className="bg-orange10" disabled>
+          <FaAngleLeft size={20} />
+        </Button>
+        <div className="space-y-12 max-w-[866px]">
+          <div className="font-bold text-">
+            <h1>Found A Roommate</h1>
+          </div>
+          <i className="text-2xl ">
+            “I’m very pleased with this website. I came across it 2022 and I had
+            no regrets whatsoever. I’ve recommended it to so many friends and
+            family. I got my roommate from here and there is no issue. I just
+            wish Studentrealestates becomes viral because it makes roommate
+            pairing easier.”
+          </i>
+        </div>
+        <Button className="bg-orange10">
+          <FaAngleRight size={20} />
+        </Button>
+      </div>
+
+      <div className="flex justify-center items-center gap-4">
+        <div>
+          <Image src={man} alt="man" />
+        </div>
+        <div>
+          <h3 className="font-bold text-2xl">Leonard Italy</h3>
+          <i className="">Chemistry Student EKSU</i>
+        </div>
       </div>
     </div>
   );
--- components/UI/SearchSchools.jsx
+++ components/UI/SearchSchools.jsx
@@ -5,17 +5,17 @@ import image3 from "../../public/assets/images/isolation mode.png";
 
 const SearchSchools = () => {
   return (
-    <div className="h-full w-full relative mt-[45rem]">
+    <div className="h-full w-full relative pt-[35rem]">
       <div>
         <Image src={image3} className="w-full h-full" alt="isolation" />
       </div>
 
-      <div className="space-y-8 h-full justify-center top-0 absolute w-full mx-auto flex flex-col">
-        <div className="text-2xl text-center font-bold">
+      <div className="space-y-10 h-full justify-center top-0 absolute w-full mx-auto flex flex-col pt-[20rem]">
+        <div className="text-2xl md:text-6xl text-center font-bold">
           <h1>University on Studentrealestates</h1>
         </div>
 
-        <div className="grid md:grid-cols-4 grid-cols-2 text-center gap-4">
+        <div className="grid md:grid-cols-4 grid-cols-2 text-center gap-8">
           <p>University of Lagos</p>
           <p>University of Lagos</p>
           <p>University of Lagos</p>
@@ -37,6 +37,12 @@ const SearchSchools = () => {
           <p>University of Lagos</p>
           <p>University of Lagos</p>
         </div>
+        <div className="text-center pt-16">
+          <input type="search" />
+
+        </div>
+
+
       </div>
     </div>
   );
