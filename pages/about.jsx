a8dadbb515aff36a6126d760a0edcbb94d47c98a
best2025j <audugodstime95@gmail.com>
2023-04-10 14:49:23 +0100

    about done

--- pages/about.jsx
+++ pages/about.jsx
@@ -6,10 +6,9 @@ import image2 from "../Assets/images/full-shot-smiley-men-sitting-curb.png";
 import image3 from "../Assets/images/two-african-woman-friends-jeans-jacket-posed-indoor-together.png";
 import frame90 from "../Assets/svg/Frame90.svg";
 import "animate.css";
-import { Carousel } from "@mantine/carousel";
-import Background_Image from "../components/Background_Image";
-import Table from "../components/Table";
-import Pagination from "../components/UI/Pagination";
+// import { Carousel } from "@mantine/carousel";
+// import Table from "../components/Table";
+// import Pagination from "../components/UI/Pagination";
 // import MdWifiFind from "react-icons/md";
 
 export const metaData = {
@@ -18,27 +17,19 @@ export const metaData = {
 
 export default function about({ props }) {
   return (
-    <div className="md:py-[4.5rem] py-5 h-full w-full">
-      <div className="relative">
-        <div className="animate__animated animate__pulse bg-gradient-to-r from-[#FE5200] to-[#00A0E3] h-full w-full">
-          <Image
-            src={image}
-            alt="ladies pix"
-            className="h-[636px] w-full md:h-auto md:w-auto mix-blend-overlay opacity-50"
-          />
-        </div>
-
-        <div className="absolute -top-2 w-full h-full left-0">
-          <div className="md:pl-10 pl-5 pt-[4px]">
+    <div>
+      <div className="md:py-[4.5rem] py-5 dark:text-black text-white h-full w-full">
+        <div className="relative">
+          <div className="animate__animated animate__pulse bg-gradient-to-r from-[#FE5200] to-[#00A0E3] h-full w-full">
             <Image
-              src={frame90}
-              alt=""
-              className="w-[136px] h-[58px] md:w-auto md:h-auto"
+              src={image}
+              alt="ladies pix"
+              className="h-[636px] w-full md:h-auto md:w-auto mix-blend-overlay opacity-50"
             />
           </div>
 
-          <div className="flex justify-center items-center animate__animated animate__backInDown inset-0 left-0 h-full flex-col text-center w-full md:space-y-[105px]">
-            <div className="max-w-xs md:max-w-[70rem] text-white">
+          <div className="flex justify-center items-center animate__animated animate__backInDown inset-0 left-0 h-full flex-col absolute top-0 text-center w-full md:space-y-[105px]">
+            <div className="max-w-xs md:max-w-[70rem] ">
               <h1 className="md:text-[3.3rem] font-bold text-[24px]">
                 Get a Roommate Of Your Choice and Make New Friend(s)
               </h1>
@@ -51,21 +42,19 @@ export default function about({ props }) {
             </div>
 
             <div className=" flex md:flex-row  flex-col md:space-x-6 space-y-4 md:space-y-0">
-              <button className="sm:w-[264px] w-[150px] h-[45px] text-white bg-orange10 sm:h-[60.1px] rounded-xl">
+              <button className="sm:w-[264px] w-[150px] h-[45px] bg-orange10 sm:h-[60.1px] rounded-xl">
                 Request Roomate
               </button>
-              <button className="sm:w-[264px] w-[150px] h-[45px] border text-orange10 border-orange10 sm:h-[60.1px] rounded-xl">
+              <button className="sm:w-[264px] w-[150px] h-[45px] border border-orange10 sm:h-[60.1px] rounded-xl">
                 Request Roomate
               </button>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex justify-center pl-[149px] px-[62px] items-center h-full w-full flex-col">
-        <div className="py-36">
-          <h1 className="text-[52px] font-bold text-center">Who We Are?</h1>
-          <p className="text-xl font-[400]">
+        <div>
+          <h1>Who We Are?</h1>
+          <p>
             Studentrealestates is one of the most admired companies in the
             academic industry of Nigeria. We offer services that are themselves,
             benchmarks in their respective categories. Today, studentrealestates
@@ -73,14 +62,14 @@ export default function about({ props }) {
             institutions.
           </p>
         </div>
-        <div className="py-[22px]">
-          <h1 className="text-[52px] font-bold text-center">Our Goal</h1>
-          <p className="text-xl font-[400]">
-            As far as students academic success is concerned, our aim is to help
-            students get settled quickly in school and make them comfortable. We
-            get students their dream roommate by finding them individuals who
-            corresponds to their lifestyle. Active since 2020, we are currently
-            expanding our services to other countries around the world.
+        <div>
+          <h1>Who We Are?</h1>
+          <p>
+            Studentrealestates is one of the most admired companies in the
+            academic industry of Nigeria. We offer services that are themselves,
+            benchmarks in their respective categories. Today, studentrealestates
+            is the undisputed leader in the pairing of roommates across Nigerian
+            institutions.
           </p>
         </div>
       </div>
@@ -175,12 +164,11 @@ export default function about({ props }) {
               </p>
             </div>
           </div>
-          
         </div>
-      </div>
-      {/*  */}
-      <div>
-        <Pagination />
+        {/*  */}
+        <div>
+          <Pagination />
+        </div>
       </div>
     </div>
   );
