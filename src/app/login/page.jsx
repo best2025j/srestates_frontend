47eef2c82a0e6f27db0b3950efad8986fb89e903
best2025j <audugodtime95@gmai.com>
2023-08-17 13:05:15 +0100

    feat: cool

---
+++ src/app/login/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Login from "../../../components/auth/Login";
+// import Login from "@/components/auth/Login";
+
+export const metaData = {
+  title: "Login",
+};
+
+const page = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default page;
